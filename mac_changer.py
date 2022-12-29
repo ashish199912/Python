7536e3461f42fb5929239aab6a278506f14cab0c
Ashish <ajha@niksun.com>
2022-12-29 16:35:13 +0530

    Mac Address Changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,15 +1,13 @@
 #!usr/bin/env python
 
 import subprocess
+import platform
 
-interface = input("interface >")
+if platform == "linux":
+    interface = input("interface >")
+    new_mac = input("new MAC >")
+    print("[+] Changing MAC address for " +interface+ " to " +new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-new_mac = input("new MAC >")
-
-print("[+] Changing MAC address for " +interface+ " to " +new_mac)
-
-subprocess.call(["ifconfig", interface, "down"])
-
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-
-subprocess.call(["ifconfig", interface, "up"])
